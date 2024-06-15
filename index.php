3eecd85104b4ca5f4dba4f1182ef251f9855896b
Roman Tyshchuk <roman.t@codelibry.com>
2024-06-15 19:47:22 +0300

    add section examples

--- index.php
+++ index.php
@@ -36,6 +36,24 @@
   </div>
 </section>
 
+<section class="about-us | section bg-primary-700 clr-neutral-000">
+  <div class="container">
+    <div class="about-us__row | row align-items-center justify-content-between">
+      <div class="about-us__content | flow col-6">
+        <h2 class="heading-2 clr-neutral-000">About us</h2>
+        <p>BrandBoosters was founded with the mission of <strong>helping businesses of all sizes</strong> achieve
+          their marketing
+          goals by working closely with each client to <strong>develop a customized strategy that delivers
+            results</strong>.</p>
+        <a href="" class="button button--accent">Learn more about us</a>
+      </div>
+      <div class="full-height-image position--right col-6" style="--space-top: 2rem">
+        <img class="bg-primary-200 round-top width-full" <?php img_src('image.png') ?> alt="">
+      </div>
+    </div>
+  </div>
+</section>
+
 </main>
 
 
--- template-parts/header/header.php
+++ template-parts/header/header.php
@@ -1,7 +1,16 @@
-<header id="header" class="header">
-  <div class="header__container">
-    <nav class="header__menu">
-      <?php wp_nav_menu(array('theme_location' => 'header-menu')); ?>
-    </nav>
+<header class="site-header">
+  <div class="container container--wide">
+    <div class="site-header__inner">
+      <img src="path/to/logo" alt="BrandBooster">
+      <nav aria-label="primary navigation">
+        <ul class="nav site-header__nav | flex-group">
+          <li><a href="#">Pricing</a></li>
+          <li><a href="#">About us</a></li>
+          <li><a href="#">Features</a></li>
+          <li><a href="#">Contact Us</a></li>
+          <li><a href="#">Careers</a></li>
+        </ul>
+      </nav>
+    </div>
   </div>
 </header>
