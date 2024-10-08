9849ad0624b935a46baf6add26b6676264f793af
tyshchukRoman <roman.t@codelibry.com>
2024-10-08 12:28:59 +0300

    acf for logo-slider

--- page.php
+++ page.php
@@ -9,8 +9,8 @@
 <main id="main" class="page">
 
 <?php
-  if (have_rows('flexible-blocks')) :
-    while (have_rows('flexible-blocks')) : the_row();
+  if (have_rows('content__page')) :
+    while (have_rows('content__page')) : the_row();
       get_template_part('blocks/' . get_row_layout());
     endwhile;
   endif;
