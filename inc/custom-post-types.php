f7b515aaad199db3700a78f948a9b518435be484
tyshchukRoman <roman.t@codelibry.com>
2024-11-30 19:10:07 +0200

    feat: add default post-types

--- inc/custom-post-types.php
+++ inc/custom-post-types.php
@@ -5,3 +5,7 @@
     Custom Post Types
 ==========================
 */
+
+
+//require get_template_directory().'/inc/post-types/member.php';
+//require get_template_directory().'/inc/post-types/testimonial.php';
