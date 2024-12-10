9e825207d4e1acb892a3c0e535bb6390988a555d
tyshchukRoman <roman.t@codelibry.com>
2024-12-10 17:23:33 +0200

    feat: add hero-1 block

--- blocks/hero.php
+++ blocks/hero.php
@@ -10,28 +10,28 @@ $button_2 = get_array_value($args, 'button_2', get_sub_field('hero__button-2'));
 $description = get_array_value($args, 'description', get_sub_field('hero__description'));
 
 if(!$title) {
-  return get_template_part('template-parts/error/invalid-block', '', [
-    'title' => 'Title is required'
-  ]);
+  return;
 }
 
 ?>
 
 <section class="hero | section">
   <div class="container">
-    <div class="row align-items-center justify-content-between g-32">
-      <div class="col-6">
-        <?php if($title): ?>
-          <div class="hero__title">
-            <?php echo $title ?>
+    <div class="hero__text">
+      <?php if($title): ?>
+        <div class="hero__title">
+          <?php echo $title ?>
+        </div>
+      <?php endif; ?>
+      
+      <div class="hero__cta">
+        <?php if($description): ?>
+          <div class="hero__description">
+            <?php echo $description ?>
           </div>
         <?php endif; ?>
-        
-        <div class="hero__description | mt-16">
-          <?php echo $description ?>
-        </div>
 
-        <div class="hero__buttons | flex-group | gx-8 mt-24">
+        <div class="hero__buttons">
           <?php if($button_1): ?>
             <a class="button button--primary" <?php acf_link_attrs($button_1) ?>>
               <?php echo $button_1['title'] ?>
@@ -45,12 +45,14 @@ if(!$title) {
           <?php endif; ?>
         </div>
       </div>
-
-      <div class="col-6">
-        <?php if($image): ?>
-          <img class="hero__image | mx-auto" <?php acf_image_attrs($image) ?>>
-        <?php endif; ?>
-      </div>
     </div>
   </div>
+
+  <?php if($image): ?>
+    <div class="container container--wide">
+      <div class="hero__banner">
+        <img <?php acf_image_attrs($image) ?> />
+      </div>
+    </div>
+  <?php endif; ?>
 </section>
